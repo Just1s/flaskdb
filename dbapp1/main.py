8fb5e65715e87a9b0e4c96c1fa42d47d0d788a20
Justas V <valantinas.justas@gmail.com>
2022-09-16 09:31:35 +0300

    žinučių filtravimas

--- dbapp1/main.py
+++ dbapp1/main.py
@@ -4,6 +4,12 @@ from flask import request, render_template
 db.create_all()
 
 
+@app.route('/')
+def index():
+    all_messages = Message.query.all()
+    return render_template('messages.html', messages_list=all_messages)
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -16,6 +22,16 @@ def login():
         return render_template('greetings.html', vardas=name)
     elif request.method == 'GET':
         return render_template('login.html')
+
+
+@app.route('/filtriuoti', methods=['GET', 'POST'])
+def filtras():
+    if request.method == 'POST':
+        email = request.form['email']
+        all_messages = Message.query.filter_by(email=email)
+        return render_template('filtruoti.html', email=email, messages_list=all_messages)
+    elif request.method == 'GET':
+        return render_template('ivest_email.html')
 
 
 if __name__ == '__main__':
